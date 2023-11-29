5b28a2de2db63b580f189da5896795dd9a9b7a95
Thomas Wilburn <twilburn@chalkbeat.org>
2023-11-29 10:49:09 -0600

    Better recursion, rearrange the steps

--- paraphrase/htmlToMarkdown.js
+++ paraphrase/htmlToMarkdown.js
@@ -1,11 +1,6 @@
-var template = document.createElement("template");
-
-export function convert(html) {
-  template.innerHTML = html.trim();
-  var walker = document.createTreeWalker(template.content);
+function recurse(node) {
   var converted = "";
-  while (walker.nextNode()) {
-    var node = walker.currentNode;
+  for (var node of node.childNodes) {
     switch (node.tagName) {
       case "H1":
       case "H2":
@@ -13,46 +8,70 @@ export function convert(html) {
       case "H4":
       case "H5":
         var level = node.tagName.match(/\d+/) * 1;
-        converted += `${"".padStart(level, "#")} ${node.textContent}\n\n`;
-        walker.nextSibling();
+        converted += `${"".padStart(level, "#")} ${recurse(node)}\n\n`;
         break;
 
       case "P":
       case "UL":
-        converted += "\n\n";
+        if (node.getAttribute("role") != "presentation") converted += "\n\n";
+        converted += recurse(node);
         break;
 
       case "LI":
-        converted += "\n* ";
+        converted += `\n* ${recurse(node)}`;
         break;
 
       case "A":
-        converted += `[${convert(node.innerHTML)}](${node.href})`;
-        walker.nextSibling();
+        converted += `[${recurse(node)}](${node.href})`;
         break;
 
-      // case "B":
-      // case "STRONG":
-      //   converted += `**${convert(node.innerHTML)}**`;
-      //   walker.nextSibling();
-      //   break;
+      case "B":
+      case "STRONG":
+        if (node.style.fontWeight == "normal") {
+          converted += recurse(node);
+        } else {
+          converted += `**${recurse(node)}**`;
+        }
+        break;
 
-      // case "I":
-      // case "EM":
-      //   converted += `*${convert(node.innerHTML)}*`;
-      //   walker.nextSibling();
-      //   break;
+      case "I":
+      case "EM":
+        converted += `*${recurse(node)}*`;
+        break;
 
-      // case "U":
-      //   converted += `_${convert(node.innerHTML)}_`;
-      //   walker.nextSibling();
-      //   break;
+      case "SPAN":
+        var nodeStack = [];
+        if (node.style.fontStyle == "italic") {
+          nodeStack.push(document.createElement("i"));
+        }
+        if (node.style.fontWeight * 1 > 400) {
+          nodeStack.push(document.createElement("b"));
+        }
+        if (node.style.textDecoration == "underline") {
+          nodeStack.push(document.createElement("u"));
+        }
+        var span = document.createElement("span");
+        var tail = nodeStack.reduce((acc, n) => acc.append(n) || n, span);
+        for (var child of node.childNodes) {
+          tail.append(child.cloneNode(true));
+        }
+        converted += recurse(span);
+        break;
 
       default:
         if (node.nodeType == Node.TEXT_NODE) {
           converted += node.textContent;
+        } else {
+          converted += recurse(node);
         }
     }
   }
+  return converted;
+}
+
+export function convert(html) {
+  var template = document.createElement("template");
+  template.innerHTML = html.trim();
+  var converted = recurse(template.content);
   return converted.replace(/^\s+$/gm, "").replace(/[\r\n]{2,}/g, "\n\n").trim();
 }
